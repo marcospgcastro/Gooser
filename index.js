86587b7cd22585085a523cb37956dc04c5485a54
marcospgcastro <marcospgcastro@fisica.if.uff.br>
2022-08-24 16:01:10 -0300

    Versão para docker container - Monitoramento de páginas públicas.

--- index.js
+++ index.js
@@ -8,8 +8,24 @@
 */
 
 const seeker = require('./lib/cronBox'); 
+const webapp = require('express');
+const status = webapp();
+// Seta alvo para monitoramento - Escolha: trends para acompanhamento da corrida presidencial de 2022.
 const target = 'https://trends24.in/brazil/';
-// const target = 'https://trends24.in/argentina'
+// Exibe status de execução ao usuário - Esta versão do Gooser será posta em um docker container!
+status.get('/', (req, res) => {
+    res.send('   Gooser está contido em um container - docker!')
+});
 
-    seeker.checkPrintSc( target, 6480, 1080, true )
-    seeker.cronoTrigger( target, 6480, 1080, true )
+status.listen(3000, '0.0.0.0', () => { 
+    // Retorna ao usuário host:porta onde é exibido status de execução.
+    console.log(' Gooser está "dockerizado" - Setado em http://0.0.0.0:3000')
+    // Executa individualmente a verificação de arquivos do diretório e cria arvore para o mesmo.
+    seeker.checkPrintSc( target, 6480, 1080, true,
+        (error) => { console.error(error)
+    });
+    // Executa de forma recursiva a verificação de atualizações na página alvo, as salvando e notificando.
+    seeker.cronoTrigger( target, 6480, 1080, true,
+        (error) => { console.error(error)
+    });
+});
