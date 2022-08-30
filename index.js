7cee891de4b60138664902e9c7ed3b7d396f6cfb
marcospgcastro <marcospgcastro@fisica.if.uff.br>
2022-08-30 15:36:28 -0300

    Mudança de 127.0.0.1 para localhost.

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const webapp = require('express');
 const status = webapp();
 const target = 'https://trends24.in/brazil';                                    // Seta alvo para monitoramento - Escolha: trends para acompanhamento da corrida presidencial de 2022.
 const port = 3000;                                                              // Porta dedicada a execução de Gooser
-const host = '127.0.0.1';                                                         // Host local, onde é exibido o status de funcionamento.
 status.get('/', (req, res) => {                                                 // Exibe status de execução ao usuário - Esta versão do Gooser será posta em um docker container!
     res.send('   Gooser está em operacional!')
 });
-status.listen(port, host, () => { 
-    console.log(' Gooser está "dockerizado" - Setado em http://'+host+':'+port) // Retorna ao usuário host:porta onde é exibido status de execução.                                   
+status.listen(port, () => { 
+    console.log(' Gooser está "dockerizado" - Setado em http://localhost:'+port) // Retorna ao usuário host:porta onde é exibido status de execução.                                   
     seeker.cronoTrigger( target, 6480, 1080, true, (error) => {                 // Executa de forma recursiva a verificação de atualizações na página alvo, as salvando e notificando.
       console.log(' >> Running, on our way, hiding!')
       throw error;  
