4a6079fe892be689dfbb64ddbe5b7bbad7c1f0a5
marcospgcastro <marcospgcastro@fisica.if.uff.br>
2022-09-01 23:29:04 -0300

    Organização de index.js - Insersção de comentários e destacamento de funcções.

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-const seeker = require('./lib/pageRun');                                        // Biblioteca pessoal que sintetiza as funcionalidades do app
+// Biblioteca pessoal que sintetiza as funcionalidades do app
+const seeker = require('./lib/pageRun');  
+
+// Biblioteca npm de recursos do nodejs                                     
 const webapp = require('express');
 const reader = require ('fs');
-const status = webapp();                                                        // Porta dedicada a execução de Gooser
+const status = webapp();   
 
-status.use(webapp.static('./usr/local/www'));                                   // Exibe status de execução ao usuário - Esta versão do Gooser será posta em um docker container!
-status.use(webapp.json());
-status.use(webapp.urlencoded({ extended: true }));
+// Exibe status de execução ao usuário
+status.use(
+  webapp.json(),
+  webapp.static('./usr/local/www'),                                             
+  webapp.urlencoded({ extended: true })
+);
 
-status.get('/status', (req, res) => {                                           // status operacional, exibe log atual em formato texto no navegador
-  if(cont = reader.readdirSync('./var/log')) {                                  // vetor responsável por ler arquivos no diretório de armazenamento de Prints
-    reader.readFile('./var/log/'+cont[cont.length-1],'utf-8', (error, data) => {
-      if (!error) {
-        res.set({ 'Content-Type': 'text/plain' })
-        res.send(data);
-      } else {
-        res.status(500).send(' Status não pode ser exibido,'+
-          'arquivo log não encontrado!');
-      }
+// status operacional, exibe log atual em formato texto no navegador  
+status.get('/status', (req, res) => { 
+
+  // Varredura de arquivos e diretórios - "var/log".
+  if(cont = reader.readdirSync('./var/log'))                                  
+    reader.readFile('./var/log/'+cont[cont.length-1],'utf-8',
+    (error, data) => {
+
+    // Seletor de texto/type  
+    res.set({
+      'Content-Type':
+      'text/plain'
+    })
+    if (!error) 
+
+      // Emissão de relatório de saída - feedback sobre o funcionamento das principais funções do app.        
+      res.send(data);
+    else
+
+      // Notificação em caso de ausência de arquivos de log
+      res.status(500)
+         .send(' Status não pode ser exibido,'
+         +'arquivo log não encontrado:  \n\n'+error);
     });
-  } else {
-    res.status(500).send(' Status não pode ser exibido,'+
-      'arvore de diretório inexistente!');
-  } 
-});
+  else
 
+  // Notificação em caso de ausência de arvore de diretórios
+  res.status(500)
+     .send(' Status não pode ser exibido,'
+     +'arvore de diretório inexistente:  \n\n'+error);
+});
 status.listen(3000, () => {
-  console.log(' Gooser está setado em http://localhost: 3000')                  // Retorna ao usuário host:porta onde é exibido status de execução.                                   
-  seeker.pageMonitor((error) => {                                               // Executa de forma recursiva a verificação de atualizações na página alvo, as salvando e notificando.
+
+  // Retorna ao usuário host:porta onde é exibido status de execução.
+  console.log(' Gooser está setado em http://localhost: 3000')
+
+  // Verificação de atualizações na página alvo, as salvando e notificando.                                                   
+  seeker.pageMonitor((error) => {                                               
     console.log(' Falha na execução de pageMonitor: '+error) 
     throw error;                   
   });                                       
